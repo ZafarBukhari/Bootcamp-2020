c805b9a801650cf30ad5db5655ca440e658d0706
Syed Zafar Abbas <bukhary72pk@yahoo.com>
2020-06-11 16:27:58 +0500

    Update App.js

--- Bootcamp_Class02/my-app/src/App.js
+++ Bootcamp_Class02/my-app/src/App.js
@@ -5,20 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Syed Zafar Abbas Bukhary</h1>
+     <h2>PIAIC Islamabad Batch-3 (Q2)</h2>
     </div>
   );
 }
